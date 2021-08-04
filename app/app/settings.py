90ae89e133dc7d75f43d718813c04ac86208a870
Jan Černý <info@jancerny.dev>
2021-08-04 10:51:09 +0200

    Added nginx and production deployment

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hh7(x*18=az1*kwlp4wxbd((5w6jus@4-t8tknd+g0&x@dfu27'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
